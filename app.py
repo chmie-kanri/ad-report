704c22a8434acd985655035adf148ec40ce38146
ori <ori@example.com>
2026-01-16 14:32:05 +0900

    2025_01_16

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ input_num = st.number_input('Input a number', value=0)
 result = input_num ** 3
 st.write('Result: ', result)
 
-st.title('Hello, Streamlit!')
- 
+
 df = pd.DataFrame({
     '名前': ['Alice', 'Bob', 'Charlie'],
     'スコア': [85, 90, 78]
@@ -27,3 +26,12 @@ df = pd.DataFrame(data, columns=['A', 'B', 'C'])
  
 # チャートの作成
 st.line_chart(df)
+
+counter = 0
+
+if 'counter' not in st.session_state:
+    st.session_state.counter = 0
+if st.button("カウンターを増やすボタン"):
+    st.session_state.counter += 1
+
+st.write("更新されたカウンター：", st.session_state.counter)
